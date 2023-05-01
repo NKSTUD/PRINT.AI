e4e2eef17671df0f9ca1c397ad6c9ae1ed41fa1d
n.kourouma@edu.umi.ac.ma <n.kourouma@edu.umi.ac.ma>
2023-05-01 04:51:57 +0100

    "style improvement"

--- my_ai/static/my_ai/my_ai.js
+++ my_ai/static/my_ai/my_ai.js
@@ -1,6 +1,3 @@
-
-
-
 function copy_text(i) {
     navigator.clipboard.writeText(document.getElementById(i).innerText).then(r => console.log(r));
     Swal.fire({
@@ -12,29 +9,14 @@ function copy_text(i) {
 
 }
 
-const copy_all_btn = document.getElementById("copy_all")
-const all_cards = document.getElementById("all_cards")
-
-function copy_all_card() {
-    console.log(copy_all_btn)
-
-}
-
-
-let user_description = document.getElementById('user_description');
-
-user_description.addEventListener('input', autoResize, false);
 
 function autoResize() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
 }
 
-testbtn = document.getElementById("test")
-testbtn.addEventListener('click', () => {
-    alert("test")
 
-})
+
 
 
 
