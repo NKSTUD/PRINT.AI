8637bd73ff4d95ecc7e5a16ece424849013bab0c
NKSTUD <n.kourouma@edu.umi.ac.ma>
2023-10-03 03:05:56 +0100

    "DATABASE URL"

--- answer_ai/settings.py
+++ answer_ai/settings.py
@@ -92,9 +92,9 @@ if DEBUG:
     }
 
 else:
-    DATABASE_URL = config('DATABASE_URL')
+
     DATABASES = {
-        "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 
 # Password validation
